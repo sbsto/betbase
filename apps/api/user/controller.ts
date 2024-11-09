35a14ad469c528c3739a010cfe33f3ea4ef70d6c
sbsto <37947148+sbsto@users.noreply.github.com>
2024-11-09 19:15:33 +0000

    Fix checks

--- apps/api/user/controller.ts
+++ apps/api/user/controller.ts
@@ -1,16 +1,16 @@
-import { api } from "encore.dev/api";
+import { api } from 'encore.dev/api';
 
 interface HelloParams {
-  name: string;
+	name: string;
 }
 
 interface HelloResponse {
-  message: string;
+	message: string;
 }
 
 export const hello = api(
-  { method: "GET", path: "/hello", expose: true },
-  async (params: HelloParams): Promise<HelloResponse> => {
-    return { message: `Hello, ${params.name}!` };
-  },
+	{ method: 'GET', path: '/hello', expose: true },
+	async (params: HelloParams): Promise<HelloResponse> => {
+		return { message: `Hello, ${params.name}!` };
+	},
 );
