0b02d97ca94ee1936e88ef542859e0ca3d51792b
sbsto <37947148+sbsto@users.noreply.github.com>
2024-11-09 19:13:27 +0000

    Add API stuff

--- apps/app/src/routes/+page.svelte
+++ apps/app/src/routes/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import { client } from '$lib/api';
+
+	let greeting = $state('');
+
+	async function handleSayHello() {
+		const res = await client.user.hello({ name: 'Sam' });
+		greeting = res.message;
+	}
 </script>
 
 <main class="px-2">
 	<h1>BetBase</h1>
+	<button onclick={handleSayHello}>Say hello to Sam</button>
+	<p>{greeting}</p>
 </main>
